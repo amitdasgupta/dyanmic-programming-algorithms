1ad12a9f8fa9e1ecf13e2605c354f7e29c3f18cc
Amit Das Gupta <amitdsgupta007@gmail.com>
2017-11-22 06:30:10 +0530

    0/1 knapsack problem using dynamic programming.

--- main.cpp
+++ main.cpp
@@ -1,30 +1,46 @@
 #include <iostream>
 
 using namespace std;
-/***********************0/1 knapack problem brute force method*///////////////////
-int knapsackProblem(int *arrweight,int *arrval,int n,int totalwieght)
+/***********************0/1 knapack problem using dynamic programming*///////////////////
+int knapsackProblem(int *arrweight,int *arrval,int n,int totalwieght,int** result)
 {
     if(n==0||totalwieght==0)
         return 0;
     else
-        if(arrweight[n-1]>totalwieght)
-        return knapsackProblem(arrweight,arrval,n-1,totalwieght);
+        {
+            if(result[n][totalwieght]>0)
+                return result[n][totalwieght];
+            if(arrweight[n-1]>totalwieght)
+        {   result[n][totalwieght]=knapsackProblem(arrweight,arrval,n-1,totalwieght,result);
+            return result[n-1][totalwieght];
+        }
     else
-        return max(knapsackProblem(arrweight,arrval,n-1,totalwieght),arrval[n-1]+knapsackProblem(arrweight,arrval,n-1,totalwieght-arrweight[n-1]));
+        {
+            result[n][totalwieght]=max(knapsackProblem(arrweight,arrval,n-1,totalwieght,result),arrval[n-1]+knapsackProblem(arrweight,arrval,n-1,totalwieght-arrweight[n-1],result));
+            return result[n][totalwieght];
+
+        }
+        }
 }
 
 int main()
 {
-    int *arrval,*arrweight,totalweight,n;
+    int *arrval,*arrweight,totalweight,n,**result;
     cout<<"enter number of weights and total weight"<<endl;
     cin>>n>>totalweight;
     arrval=new int[n];
     arrweight=new int[n];
+    result=new int*[n+1];
+    for(int i=0;i<=n;i++)
+        result[i]=new int[totalweight+1]();
     cout<<"enter weights and values "<<endl;
     for(int i=0;i<n;i++)
         cin>>arrweight[i]>>arrval[i];
-    cout<<knapsackProblem(arrweight,arrval,n,totalweight);
+    cout<<knapsackProblem(arrweight,arrval,n,totalweight,result);
     delete[] arrval;
     delete[] arrweight;
+    for(int i=0;i<=n;i++)
+        delete[] result[i];
+    delete[] result;
     return 0;
 }
