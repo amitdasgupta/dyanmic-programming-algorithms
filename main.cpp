4ce312306808ec3bacda591c0acb6f7718f5dcde
Amit Das Gupta <amitdsgupta007@gmail.com>
2018-02-07 06:13:29 +0530

    brute force Lcs

--- main.cpp
+++ main.cpp
@@ -1,69 +1,23 @@
 #include <iostream>
-
+#include<string>
 using namespace std;
-/***********************0/1 knapack problem using dynamic programming
-int knapsackProblem(int *arrweight,int *arrval,int n,int totalwieght,int** result)
+/***************Lcs*/////////////////////////////
+int Lcs(string s,string p)
 {
-    if(n==0||totalwieght==0)
-        return 0;
-    else
-        {
-            if(result[n][totalwieght]>0)
-                return result[n][totalwieght];
-            if(arrweight[n-1]>totalwieght)
-        {   result[n][totalwieght]=knapsackProblem(arrweight,arrval,n-1,totalwieght,result);
-            return result[n-1][totalwieght];
-        }
-    else
-        {
-            result[n][totalwieght]=max(knapsackProblem(arrweight,arrval,n-1,totalwieght,result),arrval[n-1]+knapsackProblem(arrweight,arrval,n-1,totalwieght-arrweight[n-1],result));
-            return result[n][totalwieght];
-
-        }
-        }
-}
-*///////////////////
-int matrixChainMultiplication(int* arr,int start,int last)
-{
-    int res=0,mini;
-    if(start==last)
+    if(s.size()==0||p.size()==0)
         return 0;
     else
     {
-        int mini=INT_MAX;
-        for(int i=start;i<last;i++)
-        {
-            res=matrixChainMultiplication(arr,start,i)+matrixChainMultiplication(arr,i+1,last)+arr[start]*arr[last+1]*arr[i+1];
-            if(res<mini)
-                mini=res;
-        }
-        return res;
+        if(s.back()==p.back())
+            return 1+Lcs(s.substr(0,s.size()-1),p.substr(0,p.size()-1));
+        else
+            return max(Lcs(s.substr(0,s.size()),p.substr(0,p.size()-1)),Lcs(s.substr(0,s.size()-1),p.substr(0,p.size())));
     }
 }
 int main()
-{   /***********knapsack code
-    int *arrval,*arrweight,totalweight,n,**result;
-    cout<<"enter number of weights and total weight"<<endl;
-    cin>>n>>totalweight;
-    arrval=new int[n];
-    arrweight=new int[n];
-    result=new int*[n+1];
-    for(int i=0;i<=n;i++)
-        result[i]=new int[totalweight+1]();
-    cout<<"enter weights and values "<<endl;
-    for(int i=0;i<n;i++)
-        cin>>arrweight[i]>>arrval[i];
-    cout<<knapsackProblem(arrweight,arrval,n,totalweight,result);
-    delete[] arrval;
-    delete[] arrweight;
-    for(int i=0;i<=n;i++)
-        delete[] result[i];
-    delete[] result;*/
-    int *arr,n;
-    cin>>n;
-    arr=new int[n+1];
-    for(int i=0;i<n;i++)
-        cin>>arr[i];
-    cout<<matrixChainMultiplication(arr,0,n-1);
+{
+    string s,p;
+    cin>>s>>p;
+    cout<<Lcs(s,p);
     return 0;
 }
